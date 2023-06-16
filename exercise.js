80b91dd19eac8d23aa8f940c75c27c45ad50dd80
Luciano <lunick997@gmail.com>
2023-06-16 12:09:33 +0200

    commit

---
+++ exercise.js
@@ -0,0 +1,18 @@
+function isAdult(person) {
+  let {id, firstName, lastName, age} = person
+  return age >= 18;
+}
+
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 17
+};
+
+
+console.log(isAdult(person));
+
+
+
+
